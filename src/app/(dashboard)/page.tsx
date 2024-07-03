60500af64843bb07d819158cd5dece449d71ce80
neoncodes <41237443+charleywolf@users.noreply.github.com>
2024-07-03 09:14:23 -0400

    feat: pages

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,88 @@
+// import ChatMessage from "@/components/ChatMessage";
+// import Wrapper from "@/components/Wrapper";
+
+// const sampleChats: (Chat & { messages: Message[] })[] = [
+//   {
+//     title: "Chat1",
+//     id: "1293",
+//     userId: "1",
+//     updatedAt: new Date(),
+//     messages: [
+//       {
+//         chatId: "1293",
+//         id: "234232",
+//         content: "Hi there! How can I assist you today?",
+//         role: "assistant",
+//       },
+//       {
+//         chatId: "1293",
+//         id: "fasdifj",
+//         content: "I need help with my project.",
+//         role: "user",
+//       },
+//       {
+//         chatId: "1293",
+//         id: "asdf24",
+//         content: "Sure, what specific issue are you facing?",
+//         role: "assistant",
+//       },
+//       {
+//         chatId: "1293",
+//         id: "12sdf",
+//         content: "I'm struggling with understanding TypeScript interfaces.",
+//         role: "user",
+//       },
+//       {
+//         chatId: "1293",
+//         id: "asdf432",
+//         content:
+//           "No problem. An interface in TypeScript defines the structure of an object. Would you like an example?",
+//         role: "assistant",
+//       },
+//     ],
+//   },
+//   {
+//     title: "Chat2",
+//     id: "23432",
+//     userId: "1",
+//     updatedAt: new Date(),
+//     messages: [
+//       {
+//         content: "Hello! What can I do for you today?",
+//         role: "assistant",
+//         chatId: "23432",
+//         id: "23ijr",
+//       },
+//       {
+//         content: "Can you tell me about mindfulness?",
+//         role: "user",
+//         chatId: "23432",
+//         id: "ppkfdg",
+//       },
+//       {
+//         content:
+//           "Of course! Mindfulness is the practice of being present in the moment. It can help reduce stress and improve mental clarity.",
+//         role: "assistant",
+//         chatId: "23432",
+//         id: "rogbj",
+//       },
+//       {
+//         content: "How can I start practicing mindfulness?",
+//         role: "user",
+//         chatId: "23432",
+//         id: "asdifgjwief",
+//       },
+//       {
+//         content:
+//           "You can start with simple breathing exercises. Focus on your breath and try to stay present without letting your mind wander.",
+//         role: "assistant",
+//         chatId: "23432",
+//         id: "fasdf",
+//       },
+//     ],
+//   },
+// ];
+
+export default function Home() {
+  return <></>;
+}
